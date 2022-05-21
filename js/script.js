863d814867caeb76d9381069aa229434e1fb7d93
maruf-mob <maruf.mob@gmail.com>
2022-05-21 18:36:48 +0600

    added a new repo named organic

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$('#counter').countdown({
+  date: '07/04/2022 23:59:59'
+  }, function () {
+    alert('Happy Deal!');
+  });
+
+//swiper slide //
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.fa-arrow-right-long',
+    prevEl: '.fa-arrow-left-long',
+  },
+  
+});
+
+//swiper navigation color
+  $('.fa-arrow-right-long').click(function(){
+    $(this).css("color","#F47606");
+    $('.fa-arrow-left-long').css("color","#363636");
+  });
+  $('.fa-arrow-left-long').click(function(){
+    $(this).css("color","#F47606");
+    $('.fa-arrow-right-long').css("color","#363636");
+  });
